6f20b9ef0b4a334293913c279ffc591237852265
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 00:41:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,11 @@
 # code here!
 class School
-  attr_accessor :roster, :name
+  attr_accessor :roster, :school
   def initialize(name)
-    @name = name
+    @school = name
     @roster = {}
+  end
+  def add_student(name, grade)
+    
   end
 end
