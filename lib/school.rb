573a222dfe3edc4813831379bd1bcc4a2b0ebe76
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 00:44:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,12 @@ class School
     @roster = {}
   end
   def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] => []
+      @roster[grade] << name
+    end
 
   end
 end
